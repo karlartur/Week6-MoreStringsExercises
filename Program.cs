a68164794fcf614bff58524ef5c9d64349d911fc
karlartur <89841872+karlartur@users.noreply.github.com>
2021-10-12 12:42:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab sõne "Hello world!" pikkust length omaduse kasutamata
+            string helloW = "Hello world!";
+            int charcounter = 0;
+            foreach (char c in helloW)
+            {
+                charcounter++;
+
+            }
+            Console.WriteLine($"Lause pikkus {helloW} on {charcounter} sümbolit.");
+        }
+    }
+    
+}
